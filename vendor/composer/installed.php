2e5fa61b8946a6c96758b66c79a95199be5a5712
Muhammad <sonofadam612@gmailcom>
2026-01-16 18:05:44 +0100

    Add Composer autoloading and initial project setup
    - Introduced composer.phar for dependency management.
    - Created vendor/autoload.php for autoloading classes.
    - Implemented Composer ClassLoader in vendor/composer/ClassLoader.php.
    - Added InstalledVersions class for tracking installed packages in vendor/composer/InstalledVersions.php.
    - Included LICENSE file for Composer.
    - Generated autoload classmap, namespaces, and PSR-4 files.
    - Created installed.json and installed.php for package metadata.
    - Set up initial project configuration for dochoax/echodoc-pdf-audio.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dochoax/echodoc-pdf-audio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '00b08b48199b38ecb29e68a01cbaffcf876b0716',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dochoax/echodoc-pdf-audio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '00b08b48199b38ecb29e68a01cbaffcf876b0716',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
