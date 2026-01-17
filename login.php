35a49c37388749c6a98f85ef5daf51b666fb4a1a
Muhammad <sonofadam612@gmailcom>
2026-01-18 00:32:22 +0100

    Fix mobile touch support for login/signup forms

--- login.php
+++ login.php
@@ -82,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css?v=3">
-    <link rel="stylesheet" href="assets/css/auth.css?v=1">
+    <link rel="stylesheet" href="assets/css/auth.css?v=2">
 </head>
 <body>
     <!-- Navigation -->
